c5a53f70ee7524cdd55e998c4577111c57230469
Nicolas Berzunces <nicoberzunces02@gmail.com>
2024-03-01 16:54:00 -0300

    proyecto final

--- js/carrito.js
+++ js/carrito.js
@@ -24,18 +24,42 @@ const pintarCarrito = () => {
         `<img src="${producto.img}"> 
         <h3>${producto.nombre}</h3>
         <p>${producto.precio} $</p>
+        <span class="restar"> - </span>
         <p>Cantidad: ${producto.cantidad}</p>
+        <span class="sumar"> + </span> 
         <p>Total: ${producto.cantidad * producto.precio}</p>
+        <span class="delete-producto"> ❌ </span> 
         `; 
     
         modalContainer.append(carritoContent)
-        console.log(carrito.length)
+        let restar = carritoContent.querySelector(".restar")
+        restar.addEventListener("click",() => {
+            if(producto.cantidad !== 1) {
+                producto.cantidad--
+            }
+            saveLocal()
+            pintarCarrito()
+        })
 
-        let eliminar = document.createElement("span")
+        let sumar = carritoContent.querySelector(".sumar")
+        sumar.addEventListener("click",() => {
+            producto.cantidad++
+            saveLocal()
+            pintarCarrito()
+        })
+
+      let eliminar = carritoContent.querySelector(".delete-producto")
+      eliminar.addEventListener("click",() => {
+        eliminarProducto(producto.id)
+      })
+
+      console.log(carrito.length)
+
+        /*let eliminar = document.createElement("span")
         eliminar.innerText = "❌"
         eliminar.className="delete-product"
         carritoContent.append(eliminar)
-        eliminar.addEventListener("click", eliminarProducto)
+        eliminar.addEventListener("click", eliminarProducto)*/
     });
     
 const total =carrito.reduce((acc, el) => acc + el.precio * el.cantidad,0);
@@ -48,16 +72,25 @@ modalContainer.append(totalBuying)
 
  verCarrito.addEventListener("click", pintarCarrito)
 
- const eliminarProducto = () => {
-        const foundId = carrito.find((element) => element.id)
+ const eliminarProducto = (id) => {
+        const foundId = carrito.find((element) => element.id === id)
+
+        console.log(foundId)
         carrito = carrito.filter((carritoId) => {
             return carritoId !== foundId
         })
         carritoCounter()
+        saveLocal()
         pintarCarrito()
  }
 
  const carritoCounter = () => {
-    cantidadCarrito.style.display = "block"
-    cantidadCarrito.innerText = carrito.length
+    cantidadCarrito.style.display ="block"
+
+    const carritoLength = carrito.length
+    localStorage.setItem("carritoLength", JSON.stringify(carritoLength))
+
+    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"))
  }
+
+ carritoCounter()
